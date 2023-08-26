edb78df929f4b58e522ca3f8f93e732a8b28d666
shohimardon296 <shohimardon296@gmail.com>
2023-08-26 19:37:44 +0500

    feature: ikkinchi va birinchi programmalarni yasadik 2

--- BirinchiProgramma.java
+++ BirinchiProgramma.java
@@ -2,13 +2,23 @@ import java.util.Scanner;
 
 public class BirinchiProgramma {
     public static void main(String[] args) {
+        String login_db = "otabekbutcher@gmail.com";
+        String password_db = "12345Password";
+
         Scanner input = new Scanner(System.in);
-        Iltimos Loginingizni kiriting!
-        // Login kiritildi
-        input.nextLine();
+
+        System.out.println("Iltimos loginingizni kiriting!");
+        String login = input.nextLine();
+        System.out.println("Iltimos parolingizni kiriting!");
+        String password = input.nextLine();
+
         input.close();
-        // Iltimos parolingizni kiriting!
-        // Parol kiritildi
-        // Siz muvaffaqiyatli dasturimizga kirdingiz!
+
+        if(login_db.equals(login) && password_db.equals(password)) {
+            System.out.println("Tabriklaymiz siz platformamizga muvaffaqiyatli kirdingiz");
+        } else {
+            System.out.println("Siz login yoki parolingizni noto'g'ri kiritdingiz!");
+        }
     }
+
 }
--- IkkinchiProgramma.java
+++ IkkinchiProgramma.java
@@ -3,7 +3,21 @@ import java.util.Scanner;
 public class IkkinchiProgramma {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.println("Iltimos ismingizni kiriting");
+        String first_name = input.nextLine();
+        System.out.println("Iltimos familiyangizni kiriting");
+        String last_name = input.nextLine();
+        System.out.println("Iltimos yoshingizni kiriting");
         int age = input.nextInt();
+        input.nextLine();
+        System.out.println("Iltimos jinsingizni kiriting");
+        String sex = input.nextLine();
+        System.out.println("Iltimos email kiriting");
+        String email = input.nextLine();
+        System.out.println("Iltimos parol kiriting");
+        String password = input.nextLine();
         input.close();
-        System.out.printf("Kiritilgan malumot %s", age);
-        {System.out.println Foydalanuvchilar bizning programmamizga o'z login va parollari bilan kirish imkoniyatiga ega bo'lishsin)}       
+        System.out.printf("Kiritilgan malumot %s,\t%s,\t%s,\t%s,\t%s,\t%s\n", first_name, last_name, age, sex, email, password);
+        System.out.println("Foydalanuvchilar bizning programmamizga o'z login va parollari bilan kirish imkoniyatiga ega bo'lishsin");
+    }
+}
